3f506c3c4dbc83ef25cfde041ef53ca91e052948
Mel Adry Olivo <meladry.olivo@students.isatu.edu.ph>
2024-12-02 12:37:19 +0800

    initial visualization

---
+++ js/holy-visualize.js
@@ -0,0 +1,113 @@
+import { show, hide, updateTableUI, updateTableDataType } from './utils.js';
+import { createDropdown } from './dropdown.js';
+import { getTableColumns, getTableData } from './holy-table.js';
+import { showSnackbar } from './snackbar.js';
+import { initPreview, previewContainer, previewTableContainer } from './holy-preview.js';
+
+const pageOverlay = document.querySelector('.page-overlay');
+const mainTotalRows = document.querySelector('.total-rows');
+const mainTable = document.querySelector('.table-main');
+
+export const handleVisualize = () => {
+  initPreview();
+
+  const cancelButton = document.querySelector('.btn[data-action="cancel"]');
+  const variableButton = document.querySelector('.btn[data-input="variable"]');
+  const splitButton = document.querySelector('.btn[data-input="split"]');
+  const mainColumnGroup = document.querySelector('.column-group-main');
+  const variableColumnGroup = document.querySelector('.column-group-variable');
+  const splitColumnGroup = document.querySelector('.column-group-split');
+  const plotDropdown = document.querySelector('.dropdown-plot');
+  const viewButton = document.querySelector('.btn[data-action="view"]');
+
+  createExploreColumns(getTableColumns(mainTable), 'main');
+
+  document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('column')) {
+      const columns = document.querySelectorAll('.column');
+      columns.forEach((col) => col.removeAttribute('selected'));
+      event.target.setAttribute('selected', '');
+
+      variableButton.classList.remove('rotate');
+      splitButton.classList.remove('rotate');
+
+      if (event.target.parentElement.classList.contains('column-group-variable')) {
+        variableButton.classList.add('rotate');
+      }
+
+      if (event.target.parentElement.classList.contains('column-group-split')) {
+        splitButton.classList.add('rotate');
+      }
+    }
+  });
+
+  variableButton.addEventListener('click', () => {
+    const columns = mainColumnGroup.querySelectorAll('.column');
+    columns.forEach((col) => {
+      if (col.hasAttribute('selected')) {
+        mainColumnGroup.removeChild(col);
+        variableColumnGroup.appendChild(createColumn(col.textContent));
+      }
+    });
+
+    const variableColumns = variableColumnGroup.querySelectorAll('.column');
+    variableColumns.forEach((c) => {
+      if (c.hasAttribute('selected')) {
+        variableColumnGroup.removeChild(c);
+        mainColumnGroup.appendChild(createColumn(c.textContent));
+      }
+    });
+
+    if (variableColumnGroup.childElementCount >= 1) {
+      plotDropdown.classList.add('selected');
+    } else {
+      plotDropdown.classList.remove('selected');
+    }
+  });
+
+  splitButton.addEventListener('click', () => {
+    const columns = mainColumnGroup.querySelectorAll('.column');
+    columns.forEach((col) => {
+      if (col.hasAttribute('selected')) {
+        mainColumnGroup.removeChild(col);
+        splitColumnGroup.appendChild(createColumn(col.textContent));
+      }
+    });
+
+    const splitColumns = splitColumnGroup.querySelectorAll('.column');
+    splitColumns.forEach((c) => {
+      if (c.hasAttribute('selected')) {
+        splitColumnGroup.removeChild(c);
+        mainColumnGroup.appendChild(createColumn(c.textContent));
+      }
+    });
+
+    if (splitColumnGroup.childElementCount >= 1) {
+      statisticsDropdown.classList.add('selected');
+    } else {
+      statisticsDropdown.classList.remove('selected');
+    }
+  });
+
+  cancelButton.addEventListener('click', () => hide(pageOverlay));
+  viewButton.addEventListener('click', async () => {});
+};
+
+export const fetchAggregatedData = async (tableData, options) => {};
+
+export const getVisualizeOptions = () => {};
+
+export const createExploreColumns = (columns, action) => {
+  columns.forEach((column) => {
+    const columnElement = createColumn(column);
+    columnElement.setAttribute('data-action', action);
+    document.querySelector(`.column-group-main`).appendChild(columnElement);
+  });
+};
+
+export const createColumn = (name) => {
+  const column = document.createElement('div');
+  column.classList.add('column');
+  column.textContent = name;
+  return column;
+};
